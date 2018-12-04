705188d123787f22b47ef0b79edf6bad3c5de113
iMac <claudio.fioretti14@gmail.com>
2018-12-04 23:45:06 +0100

    Day 2 data

---
+++ d2-1.php
@@ -0,0 +1,35 @@
+<?php
+
+$data = file_get_contents('data/d2-1.txt', true);
+
+$values = explode("\n", $data);
+$twoLetters = $threeLetters = 0;
+
+foreach ($values as $value) {
+    if(hasTwoLetters($value)) {
+        $twoLetters++;
+    }
+    elseif (hasThreeLetters($value)) {
+        $threeLetters++;
+    }
+}
+
+function hasTwoLetters($string) {
+    foreach (count_chars($string, 1) as $i => $val) {
+        if($val == 2) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function hasThreeLetters($string) {
+    foreach (count_chars($string, 1) as $val) {
+        if($val == 3) {
+            return true;
+        }
+    }
+    return false;
+}
+
+echo $twoLetters * $threeLetters;
