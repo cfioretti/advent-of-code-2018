252280c796cd249afac79ee7efe01b8951e1ad09
Claudio Fiorettti <claudio.fioretti@presstoday.com>
2018-12-21 12:18:23 +0100

    Day 3 solution 1

--- d3-1.php
+++ d3-1.php
@@ -1,10 +1,8 @@
 <?php
-// 45782 < x < 129950
 $data = file_get_contents('data/d3-1.txt', true);
 
 $values = explode("\n", $data);
 $duplicates = [];
-#1 @ 287,428: 27x20
 
 foreach ($values as $value) {
     preg_match('/#(\w+) @ (\w+),(\w+): (\w+)x(\w+)/', $value, $inches);
@@ -13,8 +11,8 @@ foreach ($values as $value) {
     $wide = $inches[4];
     $tall = $inches[5];
 
-    for ($row = $fromTheLeft; $row <= ($wide + $fromTheLeft); $row++) {
-        for ($col = $fromTheTop; $col <= ($tall + $fromTheTop); $col++) {
+    for ($row = $fromTheLeft; $row < ($wide + $fromTheLeft); $row++) {
+        for ($col = $fromTheTop; $col < ($tall + $fromTheTop); $col++) {
             if (isset($arr[$row][$col])) {
                 $duplicates[] = "$row-$col";
             }
